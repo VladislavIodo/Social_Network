0aa31fe4c248374be22398dd392e99a2132cc704
VladislavIodo <vladislav_iodo@mail.ru>
2021-01-20 20:53:19 +0300

    59_profilePage_users

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     posts: [
@@ -8,7 +9,8 @@ let initialState = {
         {id: 3, message: 'Blabla', likesCount: 11},
         {id: 4, message: 'Dada', likesCount: 11}
     ],
-    newPostText: 'it-kamasutra.com'
+    newPostText: 'it-kamasutra.com',
+    profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -20,16 +22,21 @@ const profileReducer = (state = initialState, action) => {
                 message: state.newPostText,
                 likesCount: 0
             };
-           return {
+            return {
                 ...state,
                 posts: [...state.posts, newPost],
                 newPostText: ''
             };
         }
         case UPDATE_NEW_POST_TEXT:
-            return  {
+            return {
                 ...state,
                 newPostText: action.newText
+            };
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                profile: action.profile
             };
 
         default:
@@ -39,6 +46,7 @@ const profileReducer = (state = initialState, action) => {
 
 
 export const addPostActionCreator = () => ({type: ADD_POST})
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const updateNewPostTextActionCreator = (text) =>
     ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
