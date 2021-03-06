21d47df2f48264ac8454c7eb633691ebbe70bc98
VladislavIodo <vladislav_iodo@mail.ru>
2021-03-06 11:15:44 +0300

    Updates

---
+++ precache-manifest.84caff9a2bfb1e9bd105042f76b8259e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb308c5fb8c547374b4d",
+    "url": "/Social_Network/static/css/main.5df62b3a.chunk.css"
+  },
+  {
+    "revision": "bb308c5fb8c547374b4d",
+    "url": "/Social_Network/static/js/main.bb308c5f.chunk.js"
+  },
+  {
+    "revision": "80eb49863d647161ce4c",
+    "url": "/Social_Network/static/css/1.6c77192b.chunk.css"
+  },
+  {
+    "revision": "80eb49863d647161ce4c",
+    "url": "/Social_Network/static/js/1.80eb4986.chunk.js"
+  },
+  {
+    "revision": "e00bb295d870a2cb0789",
+    "url": "/Social_Network/static/css/2.3e998bb1.chunk.css"
+  },
+  {
+    "revision": "e00bb295d870a2cb0789",
+    "url": "/Social_Network/static/js/2.e00bb295.chunk.js"
+  },
+  {
+    "revision": "85eaa403b9fad2224efb",
+    "url": "/Social_Network/static/js/3.85eaa403.chunk.js"
+  },
+  {
+    "revision": "467199897332d77770dd",
+    "url": "/Social_Network/static/js/runtime~main.46719989.js"
+  },
+  {
+    "revision": "b4b2e67b3cdda015c294c744164424fc",
+    "url": "/Social_Network/index.html"
+  }
+];
